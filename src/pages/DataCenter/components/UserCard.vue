e45c7665065a48ca5d6dc9e6d7a4895bfcf2b728
RickBm400 <rjbm29@gmail.com>
2024-04-28 00:48:21 -0500

    feat: user info card component at navbar

---
+++ src/pages/DataCenter/components/UserCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card
+    id="card_container"
+    elevation="0"
+    height="80"
+    class="d-flex justify-space-around align-center"
+  >
+    <v-avatar size="80" rounded="lg">
+      <v-img src="img/nft_logo.png" alt="kayframe"></v-img>
+    </v-avatar>
+    <v-card-text class="info">
+      <h1>{{ name }}</h1>
+      <h3 class="status">{{ status }}</h3>
+      <h3 class="location">{{ location }}</h3>
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+defineProps<{
+  name: {
+    type: String;
+    default: '';
+  };
+  status: {
+    type: String;
+    default: 'Inactive';
+  };
+  location: {
+    type: String;
+    default: '';
+  };
+}>();
+</script>
+<style lang="sass" scoped>
+#card_container
+    padding: 1rem
+    background-color: transparent
+.info
+  color: white
+  padding: 0 0 0 1rem
+  .status
+    color: #8BE79A
+  .location
+    color: #888888
+</style>
--- src/pages/DataCenter/layout.vue
+++ src/pages/DataCenter/layout.vue
@@ -1,10 +1,31 @@
+<script setup>
+import UserCard from './components/UserCard.vue';
+
+let userInfo = {
+  name: 'Darius Loca',
+  status: 'Active',
+  location: 'Camboya',
+};
+</script>
 <template>
   <div class="d-flex">
-    <nav id="navigation_container" style="width: 300px;">olas</nav>
+    <nav id="navigation_container">
+      <UserCard
+        :name="userInfo.name"
+        :location="userInfo.location"
+        :status="userInfo.status"
+      />
+    </nav>
     <router-view style="width: 100vw; height: 100vh" />
   </div>
 </template>
 <style lang="sass" scoped>
+$vertical-padding: 1rem
+
 #navigation_container
-    background-color: #202124
+    @include big-shoulders
+    background-color: #333334
+    color: white
+    width: calc( 300px - $vertical-padding )
+    padding: $vertical-padding
 </style>
