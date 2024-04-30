286b58f261a53bfbb9080cebc707f8557ff2a044
RickBm400 <rjbm29@gmail.com>
2024-04-30 18:24:00 -0500

    feat: new contacts items component

---
+++ src/pages/DataCenter/components/ContactItem.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        {{ contacts }}
+    </div>
+</template>
+<script lang="ts" setup>
+import { contacts } from "../constants/testItems"
+</script>
---
+++ src/pages/DataCenter/constants/testItems.ts
@@ -0,0 +1,55 @@
+const contacts = [
+    {
+        id: '01',
+        name: 'Darius',
+        status: 'ACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+    {
+        id: '01',
+        name: 'Andreas',
+        status: 'INACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+    {
+        id: '01',
+        name: 'Daria',
+        status: 'ACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+    {
+        id: '01',
+        name: 'Juanjo',
+        status: 'INACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+    {
+        id: '01',
+        name: 'Sebastian',
+        status: 'ACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+    {
+        id: '01',
+        name: 'Snow',
+        status: 'ACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+    {
+        id: '01',
+        name: 'Darius',
+        status: 'ACTIVE',
+        lastTime: new Date().toLocaleDateString().slice(0, 10),
+        descripcion: 'Champagne papa'
+    },
+]
+
+export {
+    contacts
+}
